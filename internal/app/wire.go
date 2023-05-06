1a77f1e05be0c4c55cc456930363878e6f401638
girihanbudi <Ghanbudi@gmail.com>
2023-05-06 23:47:20 +0700

    added app dependecy injection

---
+++ internal/app/wire.go
@@ -0,0 +1,45 @@
+//go:build wireinject
+// +build wireinject
+
+package app
+
+import (
+	env "airbnb-auth-be/internal/pkg/env/wire"
+	gorm "airbnb-auth-be/internal/pkg/gorm/wire"
+	http "airbnb-auth-be/internal/pkg/http/server/wire"
+	kafka "airbnb-auth-be/internal/pkg/kafka/wire"
+	oauth "airbnb-auth-be/internal/pkg/oauth/wire"
+
+	account "airbnb-auth-be/internal/app/account/wire"
+	auth "airbnb-auth-be/internal/app/auth/wire"
+	translation "airbnb-auth-be/internal/app/translation/wire"
+
+	svcuser "airbnb-auth-be/internal/pkg/svcuser/wire"
+
+	"github.com/google/wire"
+)
+
+var AppSet = wire.NewSet(
+	wire.Struct(new(Options), "*"),
+	wire.Struct(new(App), "*"),
+)
+
+func NewApp() (*App, error) {
+	panic(
+		wire.Build(
+			env.PackageSet,
+			gorm.PackageSet,
+			http.PackageSet,
+			kafka.PackageSet,
+			oauth.PackageSet,
+
+			svcuser.PackageSet,
+
+			AppSet,
+
+			account.ModuleSet,
+			auth.ModuleSet,
+			translation.ModuleSet,
+		),
+	)
+}
